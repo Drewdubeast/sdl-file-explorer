d1cea078768bca3b22d926a6a780bcec6bc322e9
Drew Wilken <wilk0001@stthomas.edu>
2018-12-03 12:00:34 -0600

    Visual

--- src/file-explore.cpp
+++ src/file-explore.cpp
@@ -1,10 +1,26 @@
 #include <iostream>
 #include <SDL2/SDL.h>
+#include <vector>
+#include <dirent.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <stdlib.h>
+
+typedef struct FileEntry {
+    std::string filename;
+    bool is_directory;
+} FileEntry;
 
 void EventLoop(SDL_Renderer *renderer);
-void Render(SDL_Renderer *renderer);
+void Render(SDL_Renderer *renderer, std::vector<FileEntry> currDirContents);
+void getDirectoryContents(std::string path, std::vector<FileEntry> *contents);
 
 int main(int argc, char **arv) {
+    
+    std::string homePath = getenv("HOME");
+    std::string initialPath = homePath;
+    std::vector<FileEntry> currentDirectoryContents;
+    
 	if(SDL_Init(SDL_INIT_VIDEO) != 0) {
 		fprintf(stderr, "Error, couldn't initialize SDL2\n");
 		exit(1);
@@ -18,7 +34,13 @@ int main(int argc, char **arv) {
 		exit(1);
 	}
 
-    Render(renderer);	
+    getDirectoryContents(initialPath, &currentDirectoryContents);
+    
+    for(int i = 0; i < currentDirectoryContents.size(); i++) {
+        std::cout << currentDirectoryContents[i].filename << std::endl;
+    }
+    
+    Render(renderer, currentDirectoryContents);
 	EventLoop(renderer);
 	
 	SDL_DestroyRenderer(renderer);
@@ -43,19 +65,51 @@ void EventLoop(SDL_Renderer *renderer) {
 }
 
 
-void Render(SDL_Renderer *renderer) {
-  SDL_SetRenderDrawColor(renderer, 0, 128, 128, SDL_ALPHA_OPAQUE);
-  SDL_RenderClear(renderer);
-  
-  SDL_SetRenderDrawColor(renderer, 0, 0, 0, SDL_ALPHA_OPAQUE);
+void Render(SDL_Renderer *renderer, std::vector<FileEntry> currDirContents) {
+    SDL_SetRenderDrawColor(renderer, 0, 128, 128, SDL_ALPHA_OPAQUE);
+    SDL_RenderClear(renderer);
 
-  // Create a rectangle
-  SDL_Rect rectangle;
-  rectangle.x = 100;
-  rectangle.y = 100;
-  rectangle.w = 200;
-  rectangle.h = 75;
-  SDL_RenderFillRect(renderer, &rectangle);
-   
-  SDL_RenderPresent(renderer);
+    SDL_SetRenderDrawColor(renderer, 0, 0, 0, SDL_ALPHA_OPAQUE);
+
+    for(int i = 0; i < currDirContents.size(); i++) {
+        if(currDirContents[i].is_directory) {
+            SDL_SetRenderDrawColor(renderer, 140, 160, 235, SDL_ALPHA_OPAQUE);
+        } else {
+            SDL_SetRenderDrawColor(renderer, 140, 140, 140, SDL_ALPHA_OPAQUE);
+        }
+        SDL_Rect rectangle;
+        
+        rectangle.x = 20;
+        rectangle.y = 20 + i*30;
+        rectangle.w = 30;
+        rectangle.h = 20;
+        SDL_RenderFillRect(renderer, &rectangle);
+        
+        SDL_SetRenderDrawColor(renderer, 244, 200, 66, SDL_ALPHA_OPAQUE);
+        rectangle.x = 60;
+        rectangle.y = i * 30 + 22;
+        rectangle.w = currDirContents[i].filename.length() * 10;
+        rectangle.h = 16;
+        SDL_RenderFillRect(renderer, &rectangle);
+    }
+    
+    SDL_RenderPresent(renderer);
+}
+
+void getDirectoryContents(std::string path, std::vector<FileEntry> *contents) {
+    DIR *dir;
+    struct dirent *entry;
+    
+    if((dir = opendir(path.c_str())) != NULL) {
+        
+        //Loop through each file in current directory
+        while((entry = readdir(dir)) != NULL) {
+            
+            FileEntry file;
+            file.filename = entry->d_name;
+            file.is_directory = entry->d_type == DT_DIR;
+            
+            contents->push_back(file);
+        }
+    }
 }
